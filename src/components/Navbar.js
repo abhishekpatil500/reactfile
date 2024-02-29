842d8b0a47b9dab9ff0e9f50759afc4c422c9038
abhishekpatil500 <abhishekpatil8484@gmail.com>
2024-02-29 16:30:49 +0530

    adding react all files

---
+++ src/components/Navbar.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import  PropTypes from 'prop-types'
+
+export default function Navbar(props) {
+  return (
+    <div>
+      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="/">{props.title}</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">{props.search}</a></li>
+            <li><hr class="dropdown-divider"/></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button class="btn btn-outline-success" type="submit">{props.search}</button>
+      </form>
+    </div>
+  </div>
+</nav>
+    </div>
+  )
+}
+
+Navbar.protoType={
+    title: PropTypes.string.isRequired,
+    search: PropTypes.string.isRequired
+
+}
+
+Navbar.defaultProps={
+    title: 'set title hero',
+    search:'about text here'
+}
