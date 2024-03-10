378b7cbcc1c8601b06f515d736aca08d89938586
A'I'S <147018213+abhishekpatil500@users.noreply.github.com>
2024-03-11 00:51:16 +0530

    added logic in the app

--- src/components/TextForms.js
+++ src/components/TextForms.js
@@ -9,6 +9,12 @@ export default function TextForms(props) {
     setText(newText)
   }
 
+  const handleupclicks=()=>{
+    // console.log("uppercase was clicked" + text);
+    let newText=text.toLowerCase();
+    setText(newText)
+  }
+
   const handleupchange=(event)=>{
     // console.log("on change");
     setText(event.target.value);
@@ -18,7 +24,8 @@ export default function TextForms(props) {
   // setText("new text"); correct way to change the state
   
   return (
-    <div>
+    <>
+    
       <div className="container">
         <h1>{props.heading} </h1>
         <div className="mb-3">
@@ -26,9 +33,22 @@ export default function TextForms(props) {
           <textarea className="form-control" value= {text} onChange={handleupchange}id="myBox" rows="8"></textarea>
         </div>
         <button className="btn btn-primary" onClick={handleupclick}>convert to uppercase</button>
+        <button className="btn btn-primary mx-2" onClick={handleupclicks}>convert to lowercase</button>
       </div>
-      
+       <div className="container my-3">
+        <h1>your text summary</h1>
+        <p>
+          {text.split(" ").length } words and {text.length} charaters
+
+        </p>
+        <p>{0.008 * text.split(" ").length}minutes reading time</p>
+        <h2>preview</h2>
+        <p>
+          {text}
+        </p>
+       </div>
         
-    </div>
+   
+    </>
   )
 }
