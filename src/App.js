3520c7d43b1e345f745929140a9c436887c9883d
A'I'S <147018213+abhishekpatil500@users.noreply.github.com>
2024-03-15 23:49:52 +0530

    error occures of components connection

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 
 import './App.css';
+
+// import about from './components/about';
 import Navbar from './components/Navbar';
 import TextForms from './components/TextForms';
-
+// let mystyle={
+//   color:'white',
+//   backgroundColor:'black'
+// }
 
 function App() {
   return (
     <>
-       <Navbar title="TextUtils" ></Navbar>
-      <TextForms heading="Enter the text to analyzes"></TextForms>
+         
+       <Navbar title="TextUtils" ></Navbar> 
+       <TextForms heading="Enter the text to analyzes"></TextForms>
+
+  
+  
+    
+ 
    </>
   );
 }
